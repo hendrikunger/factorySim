d2d8f21cb98601ae5b4c9a4d68230b14c726fd2f
Hendrik Unger <hendrikunger@users.noreply.github.com>
2020-03-06 11:34:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
  
 setup(name='gym_factorySim',
       version='0.1dev',
-      packages=['gym_factorySim',],
-      install_requires=['gym', 'pycairo', 'pandas', 'fabulous', 'Polygon3',]
+      packages=['gym_factorySim',]
+      
 )
