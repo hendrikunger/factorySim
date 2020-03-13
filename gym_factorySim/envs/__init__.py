c9e6d6179e91c209d8ae1a005ef4359ae68df1ce
Hendrik Unger <unger.hendrik@gmail.com>
2020-03-13 11:01:31 +0000

    Reorganized and new dependencies

--- gym_factorySim/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_factorySim.envs.factorySimEnv import FactorySimEnv
--- testAgent.py
+++ testAgent.py
@@ -4,7 +4,7 @@ import datetime as dt
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
-from gym_factorySim import FactorySimEnv
+import gym_factorySim
 import pandas as pd
 
 
