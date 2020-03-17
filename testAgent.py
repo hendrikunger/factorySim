212f0dac4ebfd9f5e94a5c6f955af9b1f0a2ac07
hendrikunger <hendrik.unger@mb.tu-chemnitz.de>
2020-03-17 18:45:12 +0100

    Make agent run again

--- testAgent.py
+++ testAgent.py
@@ -6,11 +6,23 @@ from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 import gym_factorySim
 import pandas as pd
+import os
+
+
+#filename = "Overlapp"
+#filename = "EP_v23_S1_clean"
+filename = "Simple"
+#filename = "SimpleNoCollisions"
+
+ifcpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
+  "Input",  
+  filename + ".ifc")
+
 
 
 
 # The algorithms require a vectorized environment to run
-env = gym.make('factorySimEnv-v0',inputfile = "/workspace/factorySim/Input/Simple.ifc", Loglevel=2) 
+env = gym.make('factorySimEnv-v0',inputfile = ifcpath, Loglevel=2) 
 env = DummyVecEnv([lambda: env])
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=2000)
