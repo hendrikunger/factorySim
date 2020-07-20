f2b0f27eeb32821dc867ef54f620d98ca4b81386
Hendrik Unger <unger.hendrik@gmail.com>
2020-07-20 09:44:46 +0200

    Contious Training

--- agent2.py
+++ agent2.py
@@ -73,7 +73,7 @@ def make_env(env_id, rank, ifcpath, scaling=1.0, seed=0):
 
 def prepareEnv(ifc_filename = "", objectScaling = 1.0):
 
-  num_cpu = 40  #52  # Number of processes to use
+  num_cpu = 24  #52  # Number of processes to use
   env_id = 'factorySimEnv-v0'
   if(True):
     ifcpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Input", ifc_filename)
@@ -84,119 +84,25 @@ def prepareEnv(ifc_filename = "", objectScaling = 1.0):
 
   return SubprocVecEnv([make_env(env_id, i, ifcpath, scaling=objectScaling) for i in range(num_cpu)])
 
-#---------------------------------------------------------------------------------------------------------------------
-#Training
-#---------------------------------------------------------------------------------------------------------------------
 
-if __name__ == "__main__":
-  if do_train:
-    #filename = "Overlapp"
-    filename = "Basic"
-    #filename = "EP_v23_S1_clean"
-    #filename = "Simple"
-    #filename = "SimpleNoCollisions"
-
-    #ifcpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Input", "1")
-    #check_env(gym.make('factorySimEnv-v0', inputfile = ifcpath, width=128, heigth=128, objectScaling=0.5, Loglevel=0))
-
-    # Create the vectorized environment
-    #env = prepareEnv("Basic")
-    env = prepareEnv(ifc_filename = "1", objectScaling=0.5)
-    model = PPO2(CnnLnLstmPolicy,
-        env,
-        tensorboard_log="./log/",
-        gamma=0.99, # Tradeoff between short term (=0) and longer term (=1) rewards. If to big, we are factoring in to much unnecessary info |0.99
-        n_steps=100, # | 128 
-        ent_coef=0.01,  #Speed of Entropy drop if it drops to fast, increase | 0.01 *
-        learning_rate=0.00025, # | 0.00025 *
-        vf_coef=0.5, # | 0.5
-        max_grad_norm=0.5, # | 0.5
-        lam=0.95,   #Tradeoff between current value estimate (maybe high bias) and acually received reward (maybe high variance) | 0.95
-        nminibatches=4, # | 4
-        noptepochs=4, # | 4
-        verbose=1)
-      
-    #model = PPO2.load("ppo2", env=env, tensorboard_log="./log/")
-    model.learn(total_timesteps=5000000, tb_log_name="Batch_A",reset_num_timesteps=True, callback=TensorboardCallback())
-    #model.learn(total_timesteps=1500, tb_000log_name="Basic1",reset_num_timesteps=True)
-    
-
-    #env = model.get_env()
-    done = [False for _ in range(env.num_envs)] 
-    # Passing state=None to the predict function means
-    # it is the initial state
-    state = None
-
-    prefix = 0
-    images = []
-    single_images = []
-    obs = model.env.reset()
-    img = model.env.render(mode='rgb_array')
-    single_img = env.get_images()
-
-    for i in range(100):
-      images.append(img)
-      single_images.append(single_img)
-      action, state = model.predict(obs, state=state, mask=done)
-      obs, rewards, done, info = model.env.step(action)
-      #ALl immages in one
-      #img = model.env.render(mode = 'rgb_array', prefix = "")
-      #Single Images per Environment
-      single_img = env.get_images()
-
-    #imageio.mimsave('./Output/1.gif', [np.array(img) for i, img in enumerate(images)], fps=4)
-
-    os.makedirs("./Output/1/", exist_ok=True)
-    for i, fullimage in enumerate(single_images):
-      for envid, sImage in enumerate(fullimage):
-        imageio.imsave(f"./Output/1/{envid }.{i}.png", np.array(sImage))
-
-
-    #close old env and make new one
-    env.close()
-    env = prepareEnv(ifc_filename = "1", objectScaling=0.7)
-    model.set_env(env)
-    model.learn(total_timesteps=200000, tb_log_name="Batch_B",reset_num_timesteps=True, callback=TensorboardCallback())
-    #model.learn(total_timesteps=1200000, tb_log_name="Simple1",reset_num_timesteps=True)
-
-    #env.close()
-    #env = prepareEnv("Basic")
-    #model.set_env(env)
-    #model.learn(total_timesteps=1000000, tb_log_name="Overlapp_1",reset_num_timesteps=True)
-
-    model.save("ppo2")
-    
-  else:
-    env = prepareEnv()
-    model = PPO2.load("ppo2", env=env, tensorboard_log="./log/")
-
-    
-
-#---------------------------------------------------------------------------------------------------------------------
-#Evaluation
-#---------------------------------------------------------------------------------------------------------------------
-  env.close()
-  env = prepareEnv(ifc_filename = "2", objectScaling=0.7)
-  model.set_env(env)
-
+def takePictures(model, prefix):
   #env = model.get_env()
-  done = [False for _ in range(env.num_envs)]
+  done = [False for _ in range(model.env.num_envs)]
   # Passing state=None to the predict function means
   # it is the initial state
   state = None
 
-  prefix = 0
-  images = []
+  #images = []
   single_images = []
   obs = model.env.reset()
   img = model.env.render(mode='rgb_array')
   single_img = env.get_images()
 
-  for i in range(100):
-    images.append(img)
+  for i in range(10):
+    #images.append(img)
     single_images.append(single_img)
     action, state = model.predict(obs, state=state, mask=done)
-    obs, rewards, done, info = model.env.step(action)
+    obs, _rewards, done, _info = model.env.step(action)
     #ALl immages in one
     #img = model.env.render(mode = 'rgb_array', prefix = "")
     #Single Images per Environment
@@ -205,10 +111,64 @@ if __name__ == "__main__":
 
   #imageio.mimsave('./Output/2.gif', [np.array(img) for i, img in enumerate(images)], fps=4)
 
-  os.makedirs("./Output/2/", exist_ok=True)
+  os.makedirs(f"./Output/{prefix}/", exist_ok=True)
   for i, fullimage in enumerate(single_images):
     for envid, sImage in enumerate(fullimage):
-      imageio.imsave(f"./Output/2/{envid }.{i}.png", np.array(sImage))
+      imageio.imsave(f"./Output/{prefix}/{envid }.{i}.png", np.array(sImage))
+
+#---------------------------------------------------------------------------------------------------------------------
+#Training
+#---------------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+  if do_train:
+
+    #ifcpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Input", "1")
+    #check_env(gym.make('factorySimEnv-v0', inputfile = ifcpath, width=128, heigth=128, objectScaling=0.5, Loglevel=0))
+
+    env = prepareEnv(ifc_filename = "1", objectScaling=0.5)
+    model = PPO2(CnnLnLstmPolicy,
+      env,
+      tensorboard_log="./log/",
+      gamma=0.99, # Tradeoff between short term (=0) and longer term (=1) rewards. If to big, we are factoring in to much unnecessary info |0.99
+      n_steps=100, # | 128 
+      ent_coef=0.01,  #Speed of Entropy drop if it drops to fast, increase | 0.01 *
+      learning_rate=0.00025, # | 0.00025 *
+      vf_coef=0.5, # | 0.5
+      max_grad_norm=0.5, # | 0.5
+      lam=0.95,   #Tradeoff between current value estimate (maybe high bias) and acually received reward (maybe high variance) | 0.95
+      nminibatches=4, # | 4
+      noptepochs=4, # | 4
+      verbose=1)
+    
+
+    model.learn(total_timesteps=2000, tb_log_name="Batch_1",reset_num_timesteps=True, callback=TensorboardCallback())
+    takePictures(model,1)
+    model.save(f"./models/ppo2_1")
+    #close old env and make new one
+    env.close()
+
+    for i in range(1,6):
+      env = prepareEnv(ifc_filename = str(2 - i%2), objectScaling=0.5 + i/10)
+      model.set_env(env)
+      model.learn(total_timesteps=2000, tb_log_name=f"Batch_{i+1}",reset_num_timesteps=True, callback=TensorboardCallback())
+      takePictures(model,i+1)
+      model.save(f"./models/ppo2_{i+1}")
+      env.close()
+    
+  else:
+    env = prepareEnv()
+    model = PPO2.load("./models/ppo2_", env=env, tensorboard_log="./log/")
+
+    
+
+#---------------------------------------------------------------------------------------------------------------------
+#Evaluation
+#---------------------------------------------------------------------------------------------------------------------
+  env = prepareEnv(ifc_filename = "2", objectScaling=1)
+  model.set_env(env)
+
+  takePictures(model,"X")
 
   env.close()
   
---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -R models
+rm -R Output
+
+mkdir models
+mkdir Output
