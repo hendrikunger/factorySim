53101825cad7e42d3469c83a37dbc873aec7a9b5
hendrikunger <unger.hendrik@gmail.com>
2022-05-13 14:41:39 +0200

    Refactoring KPI generation

--- kennzahlen.py
+++ kennzahlen.py
@@ -1,6 +1,4 @@
 # %%
-from ast import If
-from operator import ne
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon, LineString, MultiPoint, MultiPolygon, MultiLineString, GeometryCollection, box, mapping
@@ -9,27 +7,15 @@ from shapely.strtree import STRtree
 from shapely.ops import split, snap, voronoi_diagram, triangulate, unary_union, nearest_points
 import descartes
 import networkx as nx
+import pickle
 
 import time
-
-SAVEFILE = False 
 # %%
-def plot(multipoly:MultiPolygon, bb:MultiPolygon = None):
-    rng = np.random.default_rng()
-    fig, ax = plt.subplots(1,figsize=(16, 16))
-    #plt.axis('off')
-    plt.xlim(0,WIDTH)
-    plt.ylim(0,HEIGHT)
-    plt.autoscale(False)
-    if bb is not None:
-        ax.add_patch(descartes.PolygonPatch(bb, fc='red', ec='#000000', alpha=0.5))
+SAVEPLOT = False
+LOADDATA = True
 
-    if multi.geom_type ==  'Polygon':
-        ax.add_patch(descartes.PolygonPatch(multi, fc=rng.random(size=3), ec='#000000', alpha=0.5))
-    else:
-        for poly in multipoly.geoms:
-            ax.add_patch(descartes.PolygonPatch(poly, fc=rng.random(size=3), ec='#000000', alpha=0.5))
-    plt.show()
+# savedata = { "bounding_box": bb, "machines": multi }
+# pickle.dump( savedata, open( "FigureFactory.p", "wb" ) )
 
 
 #%% Single Machines
@@ -45,9 +31,9 @@ MAXCORNERS = 3
 WIDTH = 32
 HEIGHT = 32
 MAXSHAPEWIDTH = 4
-MAXSHAPEHEIGHT = 6
-AMOUNTRECT = 20
-AMOUNTPOLY = 10
+MAXSHAPEHEIGHT = 4
+AMOUNTRECT = 25
+AMOUNTPOLY = 5
 MAXCORNERS = 3
 
 #%% 
@@ -83,6 +69,13 @@ for x,y in lowerLeftCornersPoly:
             polygons.append(singlePoly)
     
 multi = unary_union(MultiPolygon(polygons))
+
+if LOADDATA:
+    loaddata = pickle.load( open( "FigureFactory.p", "rb" ) )
+    bb = loaddata["bounding_box"]
+    multi = loaddata["machines"]
+
+
 walkableArea = bb.difference(multi)
 
 nextTime = time.perf_counter()
@@ -154,29 +147,6 @@ starttime = nextTime
 
 
 
-# fig, ax = plt.subplots(1,figsize=(16, 16))
-# plt.xlim(0,WIDTH)
-# plt.ylim(0,HEIGHT)
-# plt.autoscale(False)
-
-
-# if multi.geom_type ==  'Polygon':
-#     ax.add_patch(descartes.PolygonPatch(multi, fc=rng.random(size=3), ec='#000000', alpha=0.5))
-# else:
-#     for poly in multi.geoms:
-#         ax.add_patch(descartes.PolygonPatch(poly, fc=rng.random(size=3), ec='#000000', alpha=0.5))
-# for line in route_lines:
-#     ax.plot(line.xy[0], line.xy[1], color='black')
-# for line in lines_touching_machines:
-#     ax.plot(line.xy[0], line.xy[1], color='green', alpha=0.5)
-# for line in lines_to_machines:
-#     ax.plot(line.xy[0], line.xy[1], color='red', alpha=0.5)
-
-# # for point in bb_points:
-# #     ax.scatter(point.xy[0], point.xy[1], color='red')
-# #ax.add_patch(descartes.PolygonPatch(allEdges, fc='blue', ec='#000000', alpha=0.5))  
-# plt.show()
-
 # Find closest points in voronoi cells
 hitpoints = points + list(MultiPoint(walkableArea.exterior.coords).geoms)
 hit_tree = STRtree(hitpoints)
@@ -226,6 +196,32 @@ starttime = nextTime
 pos=nx.get_node_attributes(G,'pos')
 
 
+fig, ax = plt.subplots(1,figsize=(16, 16))
+plt.xlim(0,WIDTH)
+plt.ylim(0,HEIGHT)
+plt.autoscale(False)
+
+
+if multi.geom_type ==  'Polygon':
+    ax.add_patch(descartes.PolygonPatch(multi, fc=rng.random(size=3), ec='#000000', alpha=0.5))
+else:
+    for poly in multi.geoms:
+        ax.add_patch(descartes.PolygonPatch(poly, fc=rng.random(size=3), ec='#000000', alpha=0.5))
+for line in route_lines:
+    ax.plot(line.xy[0], line.xy[1], color='black')
+for line in lines_touching_machines:
+    ax.plot(line.xy[0], line.xy[1], color='green', alpha=0.5)
+for line in lines_to_machines:
+    ax.plot(line.xy[0], line.xy[1], color='red', alpha=0.5)
+
+# for point in bb_points:
+#     ax.scatter(point.xy[0], point.xy[1], color='red')
+#ax.add_patch(descartes.PolygonPatch(allEdges, fc='blue', ec='#000000', alpha=0.5))  
+if SAVEPLOT: plt.savefig("Filtered_Lines.svg", format="svg")
+plt.show()
+
+
+
 
 fig, ax = plt.subplots(1,figsize=(16, 16))
 plt.xlim(0,WIDTH)
@@ -254,7 +250,7 @@ nx.draw_networkx_nodes(G, pos=pos, ax=ax, node_size=0, )
 nx.draw_networkx_edges(G, pos=pos, ax=ax, edge_color="silver", width=pathwidth * 50, edge_cmap=plt.cm.Greys, alpha=0.7)
 nx.draw_networkx_edges(G, pos=pos, ax=ax, edge_color=pathwidth, width=2, edge_cmap=plt.cm.Spectral)
 
-if SAVEFILE: plt.savefig("Pathwidth.svg", format="svg")
+if SAVEPLOT: plt.savefig("Pathwidth.svg", format="svg")
 plt.show()
 
 nextTime = time.perf_counter()
@@ -299,7 +295,7 @@ for line in route_lines:
         ax.add_patch(plt.Circle((point.x, point.y), point.distance(nearest_point), color='blue', fill=False, alpha=0.3))
     #ax.add_patch(descartes.PolygonPatch(line.buffer(1), fc="black", ec='#000000', alpha=0.5))
 
-if SAVEFILE: plt.savefig("Pathwidth_Calculation.svg", format="svg")
+if SAVEPLOT: plt.savefig("Pathwidth_Calculation.svg", format="svg")
 plt.show()
 
 
@@ -329,18 +325,26 @@ pathwidth = np.array(list((nx.get_edge_attributes(F,'pathwidth').values())))
 
 crossroads = [node for node, degree in F.degree() if degree >= 3]
 endpoints = [node for node, degree in F.degree() if degree == 1]
-nx.draw_networkx_nodes(F, pos=pos, ax=ax, nodelist=crossroads[:1], node_size=50, node_color='red')
+nx.draw_networkx_nodes(F, pos=pos, ax=ax, nodelist=crossroads, node_size=50, node_color='red')
 nx.draw_networkx_nodes(F, pos=pos, ax=ax, node_size=5, node_color='black')
-nx.draw_networkx_nodes(F, pos=pos, ax=ax, nodelist=endpoints[:1], node_size=50, node_color='blue')
+nx.draw_networkx_nodes(F, pos=pos, ax=ax, nodelist=endpoints, node_size=50, node_color='blue')
 nx.draw_networkx_edges(F, pos=pos, ax=ax, edge_color="silver", width=pathwidth * 50, edge_cmap=plt.cm.Greys, alpha=0.7)
 nx.draw_networkx_edges(F, pos=pos, ax=ax, edge_color=pathwidth, width=2, edge_cmap=plt.cm.Spectral)
 
-if SAVEFILE: plt.savefig("Deadends.svg", format="svg")
+if SAVEPLOT: plt.savefig("Deadends.svg", format="svg")
 plt.show()
 
 # %%
 
 print(nx.shortest_path_length(G, source=crossroads[0], target=endpoints[3], weight="weight"))
 # %%
+
+# %%
+
+
+
+
+
+
 
 # %%
