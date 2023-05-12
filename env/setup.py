08fab53e49c6af57d1cebf7e84eb7802df8d4ded
Hendrik Unger <unger.hendrik@gmail.com>
2023-05-12 11:28:02 +0200

    Fixed IFCopenshell requirement

--- env/setup.py
+++ env/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='local_factorySim',
       version='0.2.dev0',
       packages=find_packages(),
-      install_requires=['gym', 'pycairo', 'pandas', 'shapely', 'networkx', 'ifcopenshell']
+      install_requires=['gymnasium', 'pycairo', 'pandas', 'shapely', 'networkx', 'scipy', 'ifcopenshell']
       
 )
 
