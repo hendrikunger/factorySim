a479de9298b6ea4e3fcc598e236717bc1b5ed252
HendrikUnger <hendrik.unger@mb.tu-chemnitz.de>
2023-12-22 15:28:09 +0100

    play with timm_playground

--- env/factorySim/timm_playground.py
+++ env/factorySim/timm_playground.py
@@ -4,10 +4,11 @@ import torch
 # %%
 timm.list_models()
 # %%
-model = timm.create_model('xception41', num_classes=10, in_chans=2, img_size=256)
+model = timm.create_model('xception41', num_classes=10, in_chans=2)
 #dir(model)
 print(model.num_classes)
 print(model.num_features)
+print(model)
 # %%
 x = torch.randn(4, 2, 224, 224)
 model(x).shape
