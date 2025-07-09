1c714d06f21c4a11ad55959c80099941e9db7580
Hendrik Unger <unger.hendrik@gmail.com>
2025-07-09 14:47:59 +0200

    Created RayCluster config

---
+++ rayCluster/ray-head.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Path to your Apptainer image
+IMAGE_PATH="/home/unhe/factorySim.sif"
+
+# Ray startup parameters
+PORT=6379
+DASHBOARD_PORT=8265
+
+# Start Ray head
+apptainer exec "$IMAGE_PATH" ray start \
+    --head \
+    --port=6379 \
+    --dashboard-host=0.0.0.0 \
+    >> "$LOG_FILE" 2>&1
---
+++ rayCluster/ray-worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Path to your Apptainer image
+IMAGE_PATH="/home/unhe/factorySim.sif"
+
+# IP address of the head node (replace this with the actual IP)
+HEAD_IP="10.54.129.113"
+PORT=6379
+
+apptainer exec "$IMAGE_PATH" ray start --address="${HEAD_IP}:${PORT}"
