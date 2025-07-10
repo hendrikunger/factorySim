cfd145d113923fbe6bfb85cb3f3507c8aacc9beb
Hendrik Unger <unger.hendrik@gmail.com>
2025-07-10 13:05:24 +0200

    added craful stop

--- rayCluster/ray-stop.sh
+++ rayCluster/ray-stop.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-# List of instance names to stop
+# List of instance names to shut down gracefully
 INSTANCES=("ray-head" "ray-worker")
 
 for INSTANCE in "${INSTANCES[@]}"; do
     if apptainer instance list | grep -q "$INSTANCE"; then
+        echo "Stopping Ray inside instance: $INSTANCE"
+
+        # Gracefully shut down Ray (ignore errors if Ray isn't running)
+        apptainer exec instance://$INSTANCE ray stop || true
+
         echo "Stopping Apptainer instance: $INSTANCE"
         apptainer instance stop "$INSTANCE"
     else
