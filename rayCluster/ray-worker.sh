f32c2d1ae6d3cbdcf188b3b54ae3aabb8f4b231d
Hendrik Unger <unger.hendrik@gmail.com>
2025-07-10 13:49:46 +0200

    same for worker

--- rayCluster/ray-worker.sh
+++ rayCluster/ray-worker.sh
@@ -8,14 +8,6 @@ HEAD_NODE_IP="${RAY_HEAD_IP}"
 # Start the Apptainer instance
 apptainer instance start --writable-tmpfs "$IMAGE_PATH" "$INSTANCE_NAME"
 
-# Graceful shutdown trap
-cleanup() {
-    echo "Received SIGTERM. Shutting down Ray worker gracefully..."
-    apptainer exec instance://$INSTANCE_NAME ray stop || true
-    apptainer instance stop "$INSTANCE_NAME" || true
-    exit 0
-}
-trap cleanup SIGTERM SIGINT
 
 # Start Ray in the foreground (blocking)
 apptainer exec instance://$INSTANCE_NAME ray start \
