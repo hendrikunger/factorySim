3dcfa06c31ac16b943a300d216f9aef7ba830d4a
HendrikUnger <hendrik.unger@mb.tu-chemnitz.de>
2025-07-18 18:02:46 +0200

    p

--- rayCluster/ray-worker.sh
+++ rayCluster/ray-worker.sh
@@ -13,8 +13,7 @@ apptainer instance start  --nv --writable-tmpfs "$IMAGE_PATH" "$INSTANCE_NAME"
 # Start Ray in the foreground (blocking)
 #Disable P2P Conenction between GPUs for NCCL, as it is not supported in the cluster.
 # Disable shared memory for NCCL, as it is not supported in the cluster. Would need P2P to be enabled.
-apptainer exec \
-    --env NCCL_P2P_DISABLE=1 \   
+apptainer exec --env NCCL_P2P_DISABLE=1 \   
     --env CUDA_VISIBLE_DEVICES=0,1 \
     --env NCCL_P2P_LEVEL=LOC \
     --env NCCL_SHM_DISABLE=1 \   
