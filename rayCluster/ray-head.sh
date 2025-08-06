15271e37aaef073571351dc0871fa97891c86e3a
Hendrik Unger <unger.hendrik@gmail.com>
2025-08-06 17:08:06 +0200

    add gpu support

--- rayCluster/ray-head.sh
+++ rayCluster/ray-head.sh
@@ -6,7 +6,7 @@ INSTANCE_NAME="ray-head"
 PORT=6379
 
 # Start Apptainer instance
-apptainer instance start --writable-tmpfs "$IMAGE_PATH" "$INSTANCE_NAME"
+apptainer instance start --nv --writable-tmpfs "$IMAGE_PATH" "$INSTANCE_NAME"
 
 # Let ray handle shutdown via --block (it handles SIGTERM)
 ulimit -s 16384 && apptainer exec instance://$INSTANCE_NAME ray start \
