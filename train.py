b48ae8e29ce326d1a46464b13b6cc920da2204ea
Hendrik Unger <unger.hendrik@gmail.com>
2025-08-19 09:58:16 +0200

    gpu debug slurm

--- train.py
+++ train.py
@@ -288,9 +288,12 @@ def run():
                 "/home/sc.uni-leipzig.de/nd67ekek/factorySim/factorySim/artifacts/"],
     }
     NUMGPUS = int(os.getenv("$SLURM_GPUS",
-                0 if sys.platform == "darwin" else 1))
+                0 if sys.platform == "darwin" else 1))")
+    INDEX = (os.getenv("SLURM_STEP_GPUS"))
     
-    print(f"Using {NUMGPUS} GPUs")
+    print(f"Using {NUMGPUS} GPUs, with index {INDEX}")
+
+
     if "SLURM_JOB_UID" in os.environ or sys.platform == "darwin" or platform.node() == "pop-os":
         ray.init(num_gpus=NUMGPUS, runtime_env=runtime_env) 
     else:
