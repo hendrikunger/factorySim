3a4b247557439d42aa7c3a4288fa7155c276e16f
HendrikUnger <hendrik.unger@mb.tu-chemnitz.de>
2025-08-21 16:54:32 +0200

    Wip Curiosity

--- train.py
+++ train.py
@@ -13,12 +13,25 @@ from ray.tune import Tuner
 from ray.rllib.callbacks.callbacks import RLlibCallback
 from ray.tune import RunConfig, CheckpointConfig, TuneConfig
 from ray.rllib.algorithms.algorithm import Algorithm
+from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.algorithms.appo import APPOConfig
 from ray.rllib.algorithms.dreamerv3.dreamerv3 import DreamerV3Config
 from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
+from ray.rllib.algorithms.ppo.torch.default_ppo_torch_rl_module import DefaultPPOTorchRLModule
+from ray.rllib.core.rl_module.rl_module import RLModuleSpec, RLModule
+from ray.rllib.core.rl_module.multi_rl_module import MultiRLModuleSpec
 from ray.rllib.env.single_agent_episode import SingleAgentEpisode
-#from ray.rllib.core.rl_module.rl_module import RLModule
+from ray.rllib.examples.learners.classes.intrinsic_curiosity_learners import (
+    ICM_MODULE_ID,
+)
+from ray.rllib.examples.rl_modules.classes.intrinsic_curiosity_model_rlm import (
+    IntrinsicCuriosityModel,
+)
+from ray.rllib.examples.learners.classes.intrinsic_curiosity_learners import (
+    PPOTorchLearnerWithCuriosity,
+)
+from ray.rllib.core import DEFAULT_MODULE_ID
 #from factorySim.customRLModulTorch import MyPPOTorchRLModule
 #from factorySim.customRLModulTF import MyXceptionRLModule
 #from factorySim.customModelsTorch import MyXceptionModel
@@ -59,7 +72,7 @@ ifcpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Input", "1"
 
 
 NO_TUNE = False
-ALGO = "Dreamer"  # "Dreamer" or "PPO" or "APPO"
+ALGO = "PPO"  # "Dreamer" or "PPO" or "APPO"
 os.environ["TUNE_DISABLE_AUTO_CALLBACK_LOGGERS"] = "1"
 
 
@@ -331,32 +344,95 @@ def run():
                             vf_loss_coeff= f_config['vf_loss_coeff'],
                             train_batch_size=f_config['train_batch_size_per_learner'],
                             minibatch_size=f_config['mini_batch_size_per_learner'],
+                            learner_class=PPOTorchLearnerWithCuriosity,
+                            learner_config_dict={
+                                # Intrinsic reward coefficient.
+                                "intrinsic_reward_coeff": 0.05,
+                                # Forward loss weight (vs inverse dynamics loss). Total ICM loss is:
+                                # L(total ICM) = (
+                                #     `forward_loss_weight` * L(forward)
+                                #     + (1.0 - `forward_loss_weight`) * L(inverse_dyn)
+                                # )
+                                "forward_loss_weight": 0.2,
+            }
+
 
             ) 
 
-            algo_config.rl_module(model_config=DefaultModelConfig(
-                                                #Input is 84x84x2 output needs to be [B, X, 1, 1] for PyTorch), where B=batch and X=last Conv2D layer's number of filters
+            # algo_config.rl_module(model_config=DefaultModelConfig(
+            #                                     #Input is 84x84x2 output needs to be [B, X, 1, 1] for PyTorch), where B=batch and X=last Conv2D layer's number of filters
                                                 
-                                                conv_filters= [# [ num_filters, kernel, stride]
-                                                            [32, 5, 2],   # 128x128 → 62x62
-                                                            [48, 4, 2],   # 62x62   → 30x30
-                                                            [64, 4, 2],   # 30x30   → 14x14
-                                                            [64, 3, 2],   # 14x14   → 6x6
-                                                            [96, 3, 2],   # 6x6     → 2x2
-                                                            [96, 2, 2],   # 2x2     → 1x1
+            #                                     conv_filters= [# [ num_filters, kernel, stride]
+            #                                                 [32, 5, 2],   # 128x128 → 62x62
+            #                                                 [48, 4, 2],   # 62x62   → 30x30
+            #                                                 [64, 4, 2],   # 30x30   → 14x14
+            #                                                 [64, 3, 2],   # 14x14   → 6x6
+            #                                                 [96, 3, 2],   # 6x6     → 2x2
+            #                                                 [96, 2, 2],   # 2x2     → 1x1
 
-                                                                # [32, 8, 4],  # Reduces spatial size from 84x84 -> 20x20
-                                                                # [64, 4, 2],  # Reduces spatial size from 20x20 -> 9x9
-                                                                # [128, 3, 1],  # Reduces spatial size from 9x9 -> 7x7
-                                                                # [256, 7, 1],  # Reduces spatial size from 7x7 -> 1x1
-                                                            ],
-                                                conv_activation="relu",
-                                                head_fcnet_hiddens=[256],
-                                                vf_share_layers=True,   #Need to tune  vf_loss_coeff on Training config if you set this to True
+            #                                                     # [32, 8, 4],  # Reduces spatial size from 84x84 -> 20x20
+            #                                                     # [64, 4, 2],  # Reduces spatial size from 20x20 -> 9x9
+            #                                                     # [128, 3, 1],  # Reduces spatial size from 9x9 -> 7x7
+            #                                                     # [256, 7, 1],  # Reduces spatial size from 7x7 -> 1x1
+            #                                                 ],
+            #                                     conv_activation="relu",
+            #                                     head_fcnet_hiddens=[256],
+            #                                     vf_share_layers=True,   #Need to tune  vf_loss_coeff on Training config if you set this to True
 
-                                            ),
-                                #rl_module_spec=myRLModule,
-                                )
+            #                                 ),
+            #                     #rl_module_spec=myRLModule,
+            #                     )
+            
+
+            algo_config.rl_module(
+            rl_module_spec=MultiRLModuleSpec(
+                rl_module_specs={
+                    # The "main" RLModule (policy) to be trained by our algo.
+                    DEFAULT_MODULE_ID: RLModuleSpec(
+                        module_class=DefaultPPOTorchRLModule,
+                        model_config=DefaultModelConfig(
+                            conv_filters= [# [ num_filters, kernel, stride]
+                                    [32, 5, 2],   # 128x128 → 62x62
+                                    [48, 4, 2],   # 62x62   → 30x30
+                                    [64, 4, 2],   # 30x30   → 14x14
+                                    [64, 3, 2],   # 14x14   → 6x6
+                                    [96, 3, 2],   # 6x6     → 2x2
+                                    [128, 2, 2],   # 2x2     → 1x1
+
+                                        # [32, 8, 4],  # Reduces spatial size from 84x84 -> 20x20
+                                        # [64, 4, 2],  # Reduces spatial size from 20x20 -> 9x9
+                                        # [128, 3, 1],  # Reduces spatial size from 9x9 -> 7x7
+                                        # [256, 7, 1],  # Reduces spatial size from 7x7 -> 1x1
+                                    ],
+                            conv_activation="relu",
+                            head_fcnet_hiddens=[256],
+                            vf_share_layers=True,   #Need to tune  vf_loss_coeff on Training config if you set this to True
+                        )
+                    ),
+                    # The intrinsic curiosity model.
+                    ICM_MODULE_ID: RLModuleSpec(
+                        module_class=IntrinsicCuriosityModel,
+                        # Only create the ICM on the Learner workers, NOT on the
+                        # EnvRunners.
+                        learner_only=True,
+                        # Configure the architecture of the ICM here.
+                        model_config={
+                            "feature_dim": 128,
+                            "feature_net_hiddens": (256, 256),
+                            "feature_net_activation": "relu",
+                            "inverse_net_hiddens": (256, 256),
+                            "inverse_net_activation": "relu",
+                            "forward_net_hiddens": (256, 256),
+                            "forward_net_activation": "relu",
+                        },
+                    ),
+                }
+            ),
+            # Use a different learning rate for training the ICM.
+            algorithm_config_overrides_per_module={
+                ICM_MODULE_ID: AlgorithmConfig.overrides(lr=0.0005)
+            },
+        )
         #PPO END ------------------------------------------------------------------------------------------------------
         case "APPO":
             algo_config = APPOConfig()
@@ -462,7 +538,7 @@ def run():
     algo_config.resources(num_cpus_for_main_process=1)
     algo_config.learners(num_learners=f_config['num_learners'],
                          num_gpus_per_learner=0 if sys.platform == "darwin" else 1,
-                         num_aggregator_actors_per_learner=1,
+                         #num_aggregator_actors_per_learner=0,  #Does not work with curiosity
                          )
     algo_config.framework("torch",
                         #   torch_compile_learner=False,
