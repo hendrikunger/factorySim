064e8c71c4401c8ba27eeb31c6c3469c7de67c04
Hendrik Unger <unger.hendrik@gmail.com>
2025-12-16 15:51:37 +0100

    Wip Upload

--- geneticFactorySim.py
+++ geneticFactorySim.py
@@ -432,26 +432,11 @@ def main():
         rows = []
 
         for element in result.values():
-<<<<<<< HEAD
             if element["fitness"] < 0.8:
                 continue
             current_time = datetime.now(UTC).strftime("%Y-%m-%d %H:%M:%S")
             fullcopy = json.dumps(element.copy())
             rows.append([current_time, element["problem_id"], element["fitness"], str(element["individual"]), element["creator"], fullcopy])
-=======
-            # if element["fitness"] < 0.8:
-            #     continue
-
-            current_time = datetime.now(UTC).strftime("%Y-%m-%d %H:%M:%S")
-
-            #individual = "{-0.46084216120770133,0.3565872627555882,0.681860800927812,-0.1016739185091246,-0.8318027985174798,-0.06422719666412458,-0.6937001301831331,-0.4746699732715368,-0.831276933866309,0.46670843485339625,-0.22257866099219215,0.408166271180178,-0.7636564071679158,0.1030291710306208,0.060298744742663765}"
-            #created_at,	problem_id,	fitness,	individual,	creator,	config
-            fullcopy = json.dumps(element.copy())
-            rows.append([current_time, element["problem_id"], element["fitness"], str(element["individual"]), element["creator"], fullcopy])
-        
-
-        
->>>>>>> 04fc35c7e50dc1821a3d5074b2447a62df09700e
         if len(rows) == 0:
             print("No results to upload", flush=True)
         else:
